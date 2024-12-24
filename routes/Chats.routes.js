622a54c42d57bb2b750dcb41ec41aef5ac05fa9f
KIRANBADAKURAVA <kiranbadakurava@gmail.com>
2024-12-25 00:17:48 +0530

    refactor: clean up unused routes and import dotenv for environment variable management

--- routes/Chats.routes.js
+++ routes/Chats.routes.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import {getAllChats, createGroupChat, addParticipant, getAllMessages, deleteChat,updateGroupName,
-    removeParticipant
-} from "../controllers/Chat.controller.js";
+import {getAllChats, createGroupChat, addParticipant, getAllMessages} from "../controllers/Chat.controller.js";
 import verifyToken from "../middlewares/auth.middleware.js";
 
  
@@ -12,8 +10,5 @@ ChatRouter.route('/getallchats').get(verifyToken, getAllChats);
 ChatRouter.route('/addParticipant/:chatId').post(verifyToken, addParticipant);
 ChatRouter.route('/creategroupchat').post(verifyToken, createGroupChat);
 ChatRouter.route('/getallmessages/:chatId').get(verifyToken, getAllMessages);
-ChatRouter.route('/deletechat/:chatId').delete(verifyToken, deleteChat);
-ChatRouter.route('/updategroupname/:chatId').patch(verifyToken, updateGroupName);
-ChatRouter.route('/removeparticipant/:chatId').delete(verifyToken, removeParticipant);
 export default ChatRouter;
 
--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { UserLogin, UserLogout, UserRegistration, UpdateUserProfile,currentUserProfile, updatePassword,getAllUsers } from "../controllers/User.controller.js";
+import { UserLogin, UserLogout, UserRegistration, UpdateUserProfile,currentUserProfile, updatePassword } from "../controllers/User.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import verifyToken from "../middlewares/auth.middleware.js";
-import User from "../model/user.model.js";
 
 
 const UserRouter= Router()
@@ -21,5 +20,4 @@ UserRouter.route('/logout').post(verifyToken,UserLogout)
 UserRouter.route('/updateProfile').put(verifyToken, upload.single('profilePicture'), UpdateUserProfile)
 UserRouter.route('/getuser').get(verifyToken, currentUserProfile)
 UserRouter.route('/updatePassword').put(verifyToken, updatePassword)
-UserRouter.route('/getallusers').get(verifyToken, getAllUsers)
 export default UserRouter
