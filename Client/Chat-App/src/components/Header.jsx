5b7a68c87a7b83a03d4581a98566e9524ad59ec2
KIRANBADAKURAVA <kiranbadakurava@gmail.com>
2024-12-27 12:00:59 +0530

    Authentication slice setup

---
+++ Client/Chat-App/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+const Header = () => {
+  return (
+    <div className="header h-screen flex-col rounded-3xl">
+      <div className="header__top w-full  h-24 flex-[1] bg-blue-500 rounded-3xl">
+        </div>
+        <div>
+         {/* tools */}
+        </div>
+      
+    </div>
+  );
+};
+
+export default Header;
